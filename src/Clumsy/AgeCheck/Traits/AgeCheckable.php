575b69764415a177dd05986df6d4c0e83a6baea9
Filipe Santos <flipjms@gmail.com>
2016-07-14 12:58:19 +0100

    Extracted controller functionality to a trait; Added more options to config file; It's now easy to override the controller; Other misc improvements;

---
+++ src/Clumsy/AgeCheck/Traits/AgeCheckable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Clumsy\AgeCheck\Traits;
+
+use Clumsy\AgeCheck\Facade as AgeCheck;
+use Clumsy\Utils\Facades\Geo;
+use Validator;
+
+trait AgeCheckable
+{
+    public function ageCheck()
+    {
+        return view(config('clumsy.age-check.view'), AgeCheck::getFormData());
+    }
+
+    public function ageCheckValidateForm()
+    {
+        $rules = array(
+            'country' => 'required',
+            'day'     => 'required|integer',
+            'month'   => 'required|integer',
+            'year'    => 'required|integer',
+        );
+
+        $validator = Validator::make(request()->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->route('clumsy.age-check.validate')->withErrors($validator);
+        }
+
+        $date = request('day').'-'.request('month').'-'.request('year');
+        if (AgeCheck::checkByDate($date, request('country'))) {
+            $url = request()->session()->has('clumsy.age-check.before-url') ? 
+                    request()->session()->get('clumsy.age-check.before-url') :
+                    url(config('clumsy.age-check.success-url'));
+
+            if (request('agree') == 'check') {
+                return redirect()->to($url)->withCookie(cookie()->forever('agecheck', true));
+            }
+
+            return redirect()->to($url);
+        }
+            
+        return redirect()->to(config('clumsy.age-check.fail-url'));
+    }
+}
